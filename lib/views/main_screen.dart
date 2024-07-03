05dac76619f13f68104ff04cd8ed82a04da01bbb
zeeshan941983 <zeeshanahmad941983@gmail.com>
2024-07-04 00:46:05 +0500

    updated

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -33,104 +33,109 @@ class _HomeScreenState extends State<HomeScreen> {
     final bookheight = size.height * 0.45;
     final bookWidth = size.width * 0.7;
     return Scaffold(
+      backgroundColor: Color(0xff28282B),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: ValueListenableBuilder<double>(
-                valueListenable: _notifierScroll,
-                builder: (context, value, _) {
-                  return PageView.builder(
-                    controller: _controller,
-                    itemCount: books.length,
-                    itemBuilder: (context, index) {
-                      final book = books[index];
-                      final percent = (index - value).abs();
-                      final rotate = percent.clamp(0.0, 1.0);
-                      if (index == 1) {
-                        print(percent);
-                      }
-                      return Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: Stack(
-                                children: [
-                                  Container(
-                                    height: bookheight,
-                                    width: bookWidth,
-                                    decoration: const BoxDecoration(
-                                        color: Colors.white,
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.black26,
-                                            blurRadius: 20,
-                                            offset: Offset(5.0, 5.0),
-                                            spreadRadius: 10,
-                                          ),
-                                        ]),
-                                  ),
-                                  Transform(
-                                    alignment: Alignment.centerLeft,
-                                    transform: Matrix4.identity()
-                                      ..setEntry(3, 2, 0.002)
-                                      ..rotateY(1.2 * rotate)
-                                      ..translate(-rotate * size.width * 0.8),
-                                    child: Image.asset(
-                                      book.image,
-                                      height: bookheight,
-                                      width: bookWidth,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ],
+        child: Expanded(
+          child: ValueListenableBuilder<double>(
+            valueListenable: _notifierScroll,
+            builder: (context, value, _) {
+              return PageView.builder(
+                controller: _controller,
+                itemCount: books.length,
+                itemBuilder: (context, index) {
+                  final book = books[index];
+                  final percent = (index - value).abs();
+                  final rotate = percent.clamp(0.0, 1.0);
+
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20),
+                        Center(
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: bookheight,
+                                width: bookWidth,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black,
+                                        blurRadius: 20,
+                                        offset: Offset(5.0, 5.0),
+                                        spreadRadius: 10,
+                                      ),
+                                    ]),
                               ),
-                            ),
-                            const SizedBox(height: 10),
-                            Opacity(
-                              opacity: 1 - rotate,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    book.title,
-                                    style: const TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "By ${book.author}",
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                  Text(
-                                    books[index].price,
-                                    style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
+                              Transform(
+                                alignment: Alignment.centerLeft,
+                                transform: Matrix4.identity()
+                                  ..setEntry(3, 2, 0.002)
+                                  ..rotateY(1.2 * rotate)
+                                  ..translate(-rotate * size.width * 0.8),
+                                child: Image.asset(
+                                  book.image,
+                                  height: bookheight,
+                                  width: bookWidth,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      );
-                    },
+                        const SizedBox(height: 40),
+                        Opacity(
+                          opacity: 1 - rotate,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                book.title,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                "By ${book.author}",
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text(
+                                books[index].price,
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                fixedSize: const Size(200, 50)),
+                            onPressed: () {},
+                            child: const Text(
+                              "Buy Now",
+                              style: TextStyle(color: Colors.black),
+                            ))
+                      ],
+                    ),
                   );
                 },
-              ),
-            )
-          ],
+              );
+            },
+          ),
         ),
       ),
     );
